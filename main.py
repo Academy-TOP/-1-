2e70eeb682ee3ecb910be36f04349250ef7f666e
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-04 11:35:15 +0000

    1

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ print(list)
 list_1 = [1, 2, 3]
 list_2 = [4, 5, 6]
 
-list_1 = list_1 + list_2
+list_1.extend(list_2)
 print(list_1)
+
+list_3 = [3, 1, 2]
+list_3.sort()
+print(list_3)
 
 string = "Привет, Мир!"
 print(string)
